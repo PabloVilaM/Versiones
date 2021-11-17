700d8eebeada942741c2172f6397cf4a36d34b42
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2021-11-17 14:22:28 +0100

    Otro commit

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Peeposhy");
         System.out.println("Version 1.0");
-        System.out.println("Ya no se que añadir aqui jaja");
+        System.out.println("Una release mas");
     }
 }
